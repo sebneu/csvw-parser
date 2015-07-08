443084a7c5c4750e32ce5f1767e78c4f6a8da42a
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2015-07-08 11:49:02 +0200

    removed messytables

--- csvwparser/main.py
+++ csvwparser/main.py
@@ -1,7 +1,5 @@
 from StringIO import StringIO
 import urllib2
-import messytables
-from messytables import any_tableset, headers_guess, headers_processor, offset_processor, type_guess, types_processor
 import logging
 from csvwparser import metadata
 import metadata_extractor
@@ -11,43 +9,6 @@ __author__ = 'sebastian'
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
-
-
-def _build_tables(handle, url, date_parser):
-    table_set = any_tableset(handle)
-
-    # Build meta data objects
-    for row_set in table_set.tables:
-        # A table set is a collection of tables:
-        # guess header names and the offset of the header:
-        offset, headers = headers_guess(row_set.sample)
-        logger.debug('headers: %s', headers)
-
-        row_set.register_processor(headers_processor(headers))
-
-        # add one to begin with content, not the header:
-        row_set.register_processor(offset_processor(offset + 1))
-
-        # guess column types:
-        messytables.types.BoolType.true_values = ['true', 'True', 'TRUE']
-        messytables.types.BoolType.false_values = ['false', 'False', 'FALSE']
-        selected_types = [messytables.types.StringType,
-                          messytables.types.DecimalType,
-                          messytables.types.IntegerType,
-                          messytables.types.BoolType]
-        if date_parser:
-            selected_types.append(messytables.types.DateUtilType)
-
-        types = type_guess(row_set.sample, types=selected_types
-                           #strict=True
-                           )
-        logger.debug('types: %s', types)
-
-        # and tell the row set to apply these types to
-        # each row when traversing the iterator:
-        row_set.register_processor(types_processor(types))
-
-    return table_set
 
 
 class CSVW:
@@ -73,8 +34,7 @@ class CSVW:
         elif metadata_path:
             metadata_handle = open(metadata_path, 'rb')
 
-        # TODO
-        # self.table_set = _build_tables(handle, name, date_parsing)
+        # TODO parse table
         # TODO create embedded_metadata
         sources = metadata_extractor.metadata_extraction(url, metadata_handle)
         self.metadata = metadata.merge(sources)
